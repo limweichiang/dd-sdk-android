3c446a18aab14c7c1395bce35e95472e128fafd5
Marius Constantin <marius.constantin@datadoghq.com>
2021-11-17 15:58:06 +0100

    RUMM-1727 Introduce the WebLogsEventConsumer

--- dd-sdk-android/src/main/kotlin/com/datadog/android/rum/webview/WebLogEventConsumer.kt
+++ dd-sdk-android/src/main/kotlin/com/datadog/android/rum/webview/WebLogEventConsumer.kt
@@ -6,13 +6,51 @@
 
 package com.datadog.android.rum.webview
 
+import com.datadog.android.core.internal.persistence.DataWriter
 import com.datadog.android.core.internal.utils.sdkLogger
+import com.datadog.android.log.LogAttributes
+import com.datadog.android.log.model.LogEvent
 import com.google.gson.JsonObject
+import com.google.gson.JsonParseException
 
-internal class WebLogEventConsumer {
+internal class WebLogEventConsumer(
+    private val userLogsWriter: DataWriter<LogEvent>,
+    private val internalLogsWriter: DataWriter<LogEvent>,
+    private val rumContextProvider: WebRumEventContextProvider
+) {
 
-    fun consume(event: JsonObject) {
-        // just to make the code compile without warning. This will be removed in the next PR.
-        sdkLogger.i(event.toString())
+    fun consume(event: JsonObject, eventType: String) {
+        map(event)?.let {
+            if (eventType == INTERNAL_LOG_EVENT_TYPE) {
+                internalLogsWriter.write(it)
+            } else {
+                userLogsWriter.write(it)
+            }
+        }
+    }
+
+    private fun map(event: JsonObject): LogEvent? {
+        return try {
+            val logEvent = LogEvent.fromJson(event.toString())
+            val rumContext = rumContextProvider.getRumContext()
+            if (rumContext != null) {
+                val resolvedProperties = logEvent.additionalProperties.toMutableMap()
+                resolvedProperties[LogAttributes.RUM_APPLICATION_ID] = rumContext.applicationId
+                resolvedProperties[LogAttributes.RUM_SESSION_ID] = rumContext.sessionId
+                logEvent.copy(additionalProperties = resolvedProperties)
+            } else {
+                logEvent
+            }
+        } catch (e: JsonParseException) {
+            sdkLogger.e(JSON_PARSING_ERROR_MESSAGE, e)
+            null
+        }
+    }
+
+    companion object {
+        const val USER_LOG_EVENT_TYPE = "log"
+        const val INTERNAL_LOG_EVENT_TYPE = "internalLog"
+        const val JSON_PARSING_ERROR_MESSAGE = "The bundled web log event could not be deserialized"
+        val logEventTypes = setOf(USER_LOG_EVENT_TYPE, INTERNAL_LOG_EVENT_TYPE)
     }
 }
